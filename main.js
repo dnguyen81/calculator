e6f924a58f6097d4b2fcf7564ba47ad104d104f0
dnguyen81git <davidhuynguyen81@gmail.com>
2015-11-03 10:51:12 -0800

    basic functionality, going through test sheet

--- main.js
+++ main.js
@@ -1,71 +1,199 @@
 /**
  * Created by dndeli on 10/28/2015.
  */
+var num1 = null;
+var num2 = null;
+var op = null;
+var num_array = [''];
+var i = 0;
+var decimalAdded = false;
+//populate assigns global variables num1, num2, and op
+function populate() {
+    for (i = 0; i < num_array.length; i++) {
+        if (!isNaN(num_array[i])) {
+            if (num1 === null) {
+                num1 = num_array[i];
+            } else if (num2 === null){
+                num2 = num_array[i];
+            }
+        } else if (op === null){
+            op = num_array[i];
+        }
+    }
+    console.log("this is num1", num1, "this is num2 ", num2, "this is op ", op);
+}
 
-function displayResults(type, value, item) {
+//calculation function, uses switch and cases to determine the operand and results to perform
+function calc(num1, num2, op) {
+    var result = "";
+    var missing = 0;
+    if (num1==null && num2==null && op==null){
+        console.log("missing called", missing);
+        displayResults(missing);
 
-    console.log("type: ", type, ", value: ", value, ", item: ", item);
-    switch (value) {
+    }else{
+    switch (op) {
+
+        case '+':
+            result = parseFloat(num1) + parseFloat(num2);
+            break;
+        case '-':
+            result = parseFloat(num1) - parseFloat(num2);
+            break;
+        case 'x':
+            result = parseFloat(num1) * parseFloat(num2);
+            break;
+        case '/':
+            if (num2 === "0") {
+                num2 = "Error";
+                console.log("this is num2 ", num2);
+                displayResults(num2);
+                break;
+
+            }
+            result = parseFloat(num1) / parseFloat(num2);
+
+            break;
+
+    }
+    }
+    console.log("This is the result" + result);
+    displayResults(result);
+    var numString = result.toString();
+    return numString;
+
+
+}
+
+/*function addDot(){
+    var num = "";
+    num = $(this).text();
+    if ($('#period').on('click', 'button')){
+        input_digit(num);
+        decimalAdded = true;
+    }else if (decimalAdded==true){
+        $('#period').prop("disabled", true);
+    }
+}
+*/
+//reset global variables for further calculations
+function resetCalc(){
+    num1 = null;
+    num2 = null;
+    op = null;
+}
+//display function
+function displayResults(finish) {
+
+   // console.log("type: ", type, ", value: ", value, ", item: ", item);
+    switch (finish) {
         case undefined:
             $('#inputDisplay').html("");
             break;
         default:
-            $('#inputDisplay').html(value);
+            $('#inputDisplay').html(finish);
             break;
     }
 }
-//my_Calculator - creates a new calculator object
-var my_calculator = new calculator(displayResults);
-//after DOM load add click handlers to all buttons
-$(document).ready(function() {
-    $('button').on('click', function () {
-        var val = $(this).text();
-        console.log("btn clicked : ", val);
-        switch(val) {
-            case 'CE':
-                my_calculator.allClear();
-                break;
 
-            case 'C':
-                my_calculator.clear();
-                break;
+console.log(calc(num1, num2, op));
 
-        default:
-            my_calculator.addItem($(this).text());
-            break;
+//click function to add integers from calculator buttons
+function input_digit(number_string) {
+    num_array[i] += number_string;
+}
+//increments i for the for loop to move along the array to populate
+function increment() {
+    i++;
+    console.log("increment", i);
+}
+
+//sets increment to 0 for calculation to begin again after array is changed from calculation
+function decrement() {
+    i=0;
+
+    console.log("decrement", i);
+}
+console.log(num_array[i]);
+//clear, resets the last number to null
+function clear() {
+    displayResults('');
+    num_array.pop();
+    num_array.push('');
+}
+
+//clearAll, resets calculator to load state
+
+function clearAll() {
+    num1 = null;
+    num2 = null;
+    op = null;
+    num_array = [""];
+    i=0;
+    displayResults("0");
+}
+$(document).ready(function () {
+
+    $('#operators').on('click', 'button', function () {
+        var num = null;
+        num = $(this).text();
+        console.log("operator clicked");
+        increment(i);
+        num_array.push("");
+        input_digit(num);
+        increment(i);
+        num_array.push("");
+        displayResults(num);
+    });
+//operators click function end
+
+    $('#clearButtons, #clearAll').on('click', 'button', function () {
+        var num = $(this).text();
+        switch (num) {
+            case "C":
+                clear();
+                console.log("clear works");
+                break;
+            case "CE":
+                clearAll();
+                console.log("clearall works");
+                break;
         }
     });
+    //clear buttons end
+
+    $('.equals, #eval').on('click', 'button', function () {
+
+        var finish = "";
+
+        populate();
+
+        console.log('calculation');
+
+        finish = calc(num1, num2, op);
+
+        console.log(finish);
+
+        num_array.splice(0,3,finish);
+        decrement();
+        resetCalc();
+        return finish;
+    });
+    //equals button clicked function end
+
+    $('.keys, #numberInput').on('click', 'button', function () {
+        var num = null;
+
+        console.log('works');
+        console.log($(this).text());
+
+        num = $(this).text();
+       // addDot();
+        input_digit(num);
+        console.log(num_array[i]);
+        displayResults(num_array[i]);
+
+    });
+    //number input end
 });
-
-
-var num1 = [];
-var num2 = [];
-var value = undefined;
-//adding functionality
-var add = function(){
-
-    return num1 + num2
-};
-//subtract functionality
-var subtract = function (){
-    return num1 - num2
-};
-//division functionality
-var divide = function (){
-    return num1 / num2
-};
-//multiply functionality
-var multiply = function (){
-    return num1 * num2
-};
-//add dot functionality
-
-var Dot = function () {
-    if(value == undefined){
-        value = "0.";
-    }else{
-        my_calculator.addItem(text("."));
-    }
-
-};
-
+//document ready function end
